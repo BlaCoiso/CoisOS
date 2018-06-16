95f631ab5a8eb40db80e6997f601a329a5268237
BlaCoiso <BlaCoiso@users.noreply.github.com>
2018-06-16 17:04:55 +0100

    Updated ReadMe and added batch make script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CoisOS
-16-bit Operating System
+16-bit Operating System written in NASM
+
+## Building:
+To build, just run `make`.
+### Requirements:
+* NASM  (to assemble the source)
+* node.js (to generate the filesystem)
---
+++ make.bat
@@ -0,0 +1,9 @@
+nasm -f bin bootload.asm -o bootload.bin
+@IF /I "%ERRORLEVEL%" NEQ "0" goto :EOF
+nasm -f bin kernel.asm -o kernel.bin
+@IF /I "%ERRORLEVEL%" NEQ "0" goto :EOF
+nasm -f bin test.asm -o test.bin
+@IF /I "%ERRORLEVEL%" NEQ "0" goto :EOF
+node makefs
+@IF /I "%ERRORLEVEL%" NEQ "0" goto :EOF
+copy /Y/B bootload.bin+fat.bin+rootdir.bin+fs_data.bin output.bin
